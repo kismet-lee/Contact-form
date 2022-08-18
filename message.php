554eb31283b317ae77a0a27db96422f9a1eba725
Destiny Simmons <34193988+kismet-lee@users.noreply.github.com>
2022-08-18 23:40:36 +0900

    Added error messages to js and php
    The error messages don't seem to be changing though. I will diagnose.

--- message.php
+++ message.php
@@ -1,11 +1,25 @@
 <?php
-echo "This msg is sent from PHP file";
 $name = htmlspecialchars ($_POST['name']);
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$website = $_POST['website'];
-$message = $_POST['message'];
+$email = htmlspecialchars ($_POST['email']);
+$phone = htmlspecialchars($_POST['phone']);
+$website = htmlspecialchars($_POST['website']);
+$message = htmlspecialchars($_POST['message']);
 
-echo $name;
-echo $email;
+if (!empty($email) && !empty($message)){ // if the email and message fields are not empty
+    if(filter_var($email, FILTER_VALIDATE_EMAIL)){ // if user entered email is valid
+    $receiver = "receive_email_address";
+    $subject = "From: $name <$email>";
+    $body = "Name: $name\nEmail: $email\nPhone: $phone\nWebsite: $website\n\n Message:\n$message\n\nRegards,\n$name";
+    $sender = "From $email";
+    if (mail ($receiver, $subject, $body, $sender)){
+    echo "Your message has been sent";
+    }else{
+    echo "Sorry, failed to send your message!";
+    }
+    }else{
+    echo "Enter a valid email address!";
+    }
+}else{
+    echo "Email and password field are required!";
+}
 ?>
--- script.js
+++ script.js
@@ -3,7 +3,10 @@ const form = document.querySelector('form'),
 
 form.onsubmit = (e) => {
   e.preventDefault(); //prevent form from submitting
+  statusTxt.style.color = '#fef7e5';
   statusTxt.style.display = 'block';
+  statusTxt.innerText = 'Sending your message...';
+  form.classList.add('disabled');
 
   let xhr = new XMLHttpRequest(); //creating new xml object
   xhr.open('POST', 'message.php', true); //sending post request to message.php file
@@ -12,7 +15,20 @@ form.onsubmit = (e) => {
     if (xhr.readyState == 4 && xhr.status == 200) {
       //if ajax response status is 200 & ready status is 4 means there is no error
       let response = xhr.response; //storing ajax response in a response variable
-      console.log(response);
+      if (
+        response.indexOf('required') != -1 ||
+        response.indexOf('valid') != -1 ||
+        response.indexOf('failed') != -1
+      ) {
+        statusTxt.style.color = 'red';
+      } else {
+        form.reset();
+        setTimeout(() => {
+          statusTxt.style.display = 'none';
+        }, 3000);
+      }
+      statusTxt.innterText = response;
+      form.classList.remove('disabled');
     }
   };
   let formData = new FormData(form); // creating new FormData obj. This obj is used to send form data
