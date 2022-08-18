966ff3f45ff9e98a9d767f721992cef0dbd30c61
Destiny Simmons <34193988+kismet-lee@users.noreply.github.com>
2022-08-18 16:26:31 +0900

    Added JS and PHP files

---
+++ script.js
@@ -0,0 +1,19 @@
+const form = document.querySelector('form'),
+  statusTxt = form.querySelector('.button-area span');
+
+form.onsubmit = (e) => {
+  e.preventDefault(); //prevent form from submitting
+  statusTxt.style.display = 'block';
+
+  let xhr = new XMLHttpRequest(); //creating new xml object
+  xhr.open('POST', 'message.php', true); //sending post request to message.php file
+  xhr.onload = () => {
+    //once ajax loaded
+    if (xhr.readyState == 4 && xhr.status == 200) {
+      //if ajax response status is 200 & ready status is 4 means there is no error
+      let response = xhr.response; //storing ajax response in a response variable
+      console.log(response);
+    }
+  };
+  xhr.send();
+};
